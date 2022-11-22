3b47b2cfc64812712d500430fb56be5dc988d194
M. Farhan Tandia <farhantandia@gmail.com>
2022-11-22 10:23:42 +0800

    Begin

---
+++ BookStoreWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
