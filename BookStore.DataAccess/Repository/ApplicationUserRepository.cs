cf2f4679a403b9e0da667023250861cbfa71ef31
Farhan Tandia <farhantandia.eic08g@nctu.edu.tw>
2023-01-13 10:05:48 +0800

    Add Items to ShoppingCart

---
+++ BookStore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDBContext _db;
+
+        public ApplicationUserRepository(ApplicationDBContext db):base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
--- BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models;
 using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BookStoreWeb.Controllers
 {
@@ -24,15 +26,43 @@ namespace BookStoreWeb.Controllers
         }
 
 
-        public IActionResult Details(int id)
+        public IActionResult Details(int productId)
         {
 
             ShoppingCart cartObj = new()
             {
-                Count = 1, Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
-        };
+                Count = 1,
+                ProductId = productId,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category,CoverType"),
+            };
 
             return View(cartObj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            shoppingCart.ApplicationUserId = claim.Value;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+                u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
+
+
+            if (cartFromDb == null)
+            {
+
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
